c70acfa8dfa9a65bfb7826e9718efa0b09e44ebf
Christopher Crocker <chris.crocker7227@gmail.com>
2025-03-08 10:58:49 -0500

    Module 06-streaming

---
+++ 06-streaming/pyflink/src/producers/load_taxi_data.py
@@ -0,0 +1,33 @@
+import csv
+import json
+from time import time
+from kafka import KafkaProducer
+
+def main():
+    t0 = time()
+    # Create a Kafka producer
+    producer = KafkaProducer(
+        bootstrap_servers='localhost:9092',
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+
+    csv_file = 'data/green_tripdata_2019-10.csv'  # change to your CSV file path if needed
+
+    with open(csv_file, 'r', newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            # Each row will be a dictionary keyed by the CSV headers
+            # Send data to Kafka topic "green-data"
+            producer.send('green-data', value=row)
+
+    # Make sure any remaining messages are delivered
+    producer.flush()
+    t1 = time()
+    took = t1 - t0
+    print(took)
+    producer.close()
+
+
+if __name__ == "__main__":
+    main()
